734d0cdbac738948630719b2c7a722d6e00ca4bd
Irrevaldy <irrevaldy@gmail.com>
2018-03-23 18:09:38 +0700

    c

--- resources/views/corporate_merchant.blade.php
+++ resources/views/corporate_merchant.blade.php
@@ -262,7 +262,7 @@
 
                     <script type="text/javascript">
 
-                      function setFileNameMerchant() {
+                      function setFileNameMerchanta() {
                         var input = document.getElementById("logoBrowseo");
                         var logoText = document.getElementById("logoTexto");
                         var browseFile = document.getElementById("browseFileo");
