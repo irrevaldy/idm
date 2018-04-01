a687c686095bf7115b15b50eed80282aa152dc26
irrevaldy <irrevaldy@gmail.com>
2018-04-02 06:26:47 +0700

    admin bracket gone

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
+    <div class="header" style="display:none;">
+        <h2><i class="fa fa-th" aria-hidden="true"></i><strong>Home</strong></h2>
+    </div>
+    <div class="row" style="display:none;">
+        <div class="col-lg-12" style="height:50px">
+          <b>Summary EDC Monitoring</b>
+        </div>
+        <div class="col-lg-12" style="height:50px">
+          Total EDC : {{ $total_edc }}
+        </div>
+        <div class="col-lg-12" style="height:50px">
+          EDC Active :{{ $total_active }}
+        </div>
+        <div class="col-lg-12" style="height:50px">
+          EDC Non Active : {{ $total_not_active }}
+        </div>
+        <div class="col-lg-12" style="height:50px">
+          EDC Active Transaction : {{ $total_active_transaction }}
+        </div>
+    </div>
+
     <div class="content-wrapper"><!-- Content Wrapper. Contains page content -->
 
       <!-- Content Header (Page header) -->
